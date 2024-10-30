a988a4ba018d3349468dbcbd3de0e34eb9408bcc
Utsha Samanta <utshalm@gmail.com>
2024-10-30 15:17:02 -0600

    Fixed entrant activitu UI, Added QR code scanner, Changed User object, Working on notification but buggy

--- app/src/main/java/com/example/myapplication/entrant/EntrantMainActivity.java
+++ app/src/main/java/com/example/myapplication/entrant/EntrantMainActivity.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -21,7 +22,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class EntrantMainActivity extends AppCompatActivity {
@@ -29,7 +34,9 @@ public class EntrantMainActivity extends AppCompatActivity {
     private FirebaseFirestore db; // Firebase Firestore database object for retrieving user data
     private TextView welcomeText; // TextView to display the welcome message with the user's name
     private ImageView profilePic; // ImageView to display the user's profile picture
-    private Button updateProfileBtn; // Button for users to navigate to the update profile screen
+    private Button notificationBtn;
+    private Button qrCodeBtn;
+    private ImageView updateProfile;
     private String device;
 
 
@@ -45,16 +52,33 @@ public class EntrantMainActivity extends AppCompatActivity {
         // Initialize the UI elements
         welcomeText = findViewById(R.id.welcomeTextView); // Find the TextView by its ID to display the welcome message
         profilePic = findViewById(R.id.welcomeProfilePictureMain); // Find the ImageView by its ID for profile picture display
-        updateProfileBtn = findViewById(R.id.updateProfileBtn); // Find the Button by its ID for navigating to update profile screen
+        notificationBtn = findViewById(R.id.notificationBtn);
+        qrCodeBtn = findViewById(R.id.qrCodeBtn);
+        updateProfile = findViewById(R.id.updateProfileImg);
 
         device = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
 
         getData(device);
 
         // Set a click listener on the update profile button
-        updateProfileBtn.setOnClickListener(view -> {
+        updateProfile.setOnClickListener(view -> {
             startActivity(new Intent(EntrantMainActivity.this, UpdateProfileActivity.class)); // Navigate to the update profile screen
             finish(); // Close the MainActivity
+        });
+
+        qrCodeBtn.setOnClickListener(view -> {
+            ScanOptions options = new ScanOptions();
+            options.setPrompt("Scan QR code");
+            options.setBeepEnabled(true);
+            options.setOrientationLocked(true);
+            options.setCaptureActivity(CaptureAct.class);
+
+            barcodeLauncher.launch(options);
+        });
+
+        notificationBtn.setOnClickListener(view -> {
+            startActivity(new Intent(EntrantMainActivity.this, NotificationActivity.class));
+            finish();
         });
 
         // Initialize the button to navigate to MyEventActivity
@@ -83,12 +107,9 @@ public class EntrantMainActivity extends AppCompatActivity {
                             String profilePicUrl = document.getString("Profile Picture"); // Retrieve the URL of the user's profile picture
                             welcomeText.setText("Welcome " + firstname + " " + lastname); // Set the welcome text with the user's full name
 
-                            // Load the user's profile picture using Glide, a third-party image loading library
-                            Glide.with(EntrantMainActivity.this)
-                                    .load(profilePicUrl) // Load the image from the URL obtained from Firestore
-                                    .placeholder(R.drawable.baseline_person_outline_24) // Display a default placeholder while the image loads
-                                    .error(R.drawable.baseline_person_outline_24) // Show a default image if loading the picture fails
-                                    .into(profilePic); // Set the loaded image into the ImageView
+                            setImageInView(profilePic, profilePicUrl);
+                            setImageInView(updateProfile, profilePicUrl);
+
                         } else {
                             Log.d("MainActivity", "No such document"); // Log a message if the document does not exist
                         }
@@ -97,5 +118,22 @@ public class EntrantMainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    private void setImageInView(ImageView view, String picUrl) {
+        // Load the user's profile picture using Glide, a third-party image loading library
+        Glide.with(EntrantMainActivity.this)
+                .load(picUrl) // Load the image from the URL obtained from Firestore
+                .placeholder(R.drawable.baseline_person_outline_24) // Display a default placeholder while the image loads
+                .error(R.drawable.baseline_person_outline_24) // Show a default image if loading the picture fails
+                .into(view); // Set the loaded image into the ImageView
+    }
+
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents() == null) {
+            Toast.makeText(EntrantMainActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(EntrantMainActivity.this, "Scanned: " + result.getContents(), Toast.LENGTH_SHORT).show();
+        }
+    });
 
 }
--- app/src/main/java/com/example/myapplication/objects/Users.java
+++ app/src/main/java/com/example/myapplication/objects/Users.java
@@ -1,13 +1,18 @@
 package com.example.myapplication.objects;
 
+import java.util.ArrayList;
+
 public class Users {
+    private String userId;
     private String firstName;
     private String lastName;
     private String email;
     private String profilePicture;
     private String role;
+    private ArrayList<String> eventList = new ArrayList<>();
 
-    public Users(String firstName, String lastName, String email, String profilePicture, String role) {
+    public Users(String userId, String firstName, String lastName, String email, String profilePicture, String role) {
+        this.userId = userId;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -15,27 +20,71 @@ public class Users {
         this.role = role;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String id) {
+        this.userId = id;
+    }
+
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getProfilePicture() {
         return profilePicture;
     }
 
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
     public String getRole() {
         return role;
     }
 
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public ArrayList<String> getEventList () {
+        return this.eventList;
+    }
+
+    public void setEventList(ArrayList<String> eventList) {
+        this.eventList = eventList;
+    }
+
     public String getName() {
         return firstName + ' ' + lastName;
+    }
+
+    public void addEvent(String eventId) {
+        this.eventList.add(eventId);
+    }
+
+    public void removeEvent (String eventId) {
+        this.eventList.remove(eventId);
     }
 }
