09d686575c1eefda16673db460022e19c66ba785
Haoyan Yin <1453488395@qq.com>
2024-11-08 01:07:14 -0700

    Updated some javadoc

--- app/src/main/java/com/example/myapplication/organizer/EventEditActivity.java
+++ app/src/main/java/com/example/myapplication/organizer/EventEditActivity.java
@@ -1,5 +1,6 @@
 /**
- * Activity for editing existing event
+ * Activity for editing existing event, involving database information update,
+ * poster update and event deletion
  */
 package com.example.myapplication.organizer;
 
@@ -123,6 +124,7 @@ public class EventEditActivity extends AppCompatActivity {
 
     /**
      * This method allows the user to select an image from their device
+     * and asks for device permission
      */
     private void openFileChooser() {
         Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -198,7 +200,8 @@ public class EventEditActivity extends AppCompatActivity {
     }
 
     /**
-     * This method updates the event
+     * This method updates the event for all the editable fields and make instant update
+     * on the firebase for the event
      */
     private void updateEvent() {
         String updatedTitle = editTextTitle.getText().toString();
